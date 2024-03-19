b9e091d3f2e01c532e99f46dc88c0518be802ba3
Alexis <alexismoreauavila@gmail.com>
2024-03-19 23:00:05 +0100

    ordering

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Navbar from "./assets/Components/NavBar/Footer/NavBar/Navbar";
-import Footer from "./assets/Components/NavBar/Footer/Footer";
+import Navbar from "./assets/Components/NavBar/NavBar";
+import Footer from "./assets/Components/Footer/Footer";
 import SideBar from "./assets/Components/SideBar/SideBar";
 import "./App.css";
 function App() {
